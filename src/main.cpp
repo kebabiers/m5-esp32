f46b1ca55a19e210e241bec89f98bc1b6e33d0b4
Bastien <mrdzaack@dzkcorp.fr>
2021-10-28 12:13:59 +0200

    API + WiFi + NTP

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,7 @@
 WiFiClient client;
 HTTPClient http;
 
+//Identifiant et mot de passe du partage de connexion
 const char *ssid = "iPhone";
 const char *pass = "sech38dzk";
 
@@ -19,7 +20,6 @@ unsigned long getTime() {
   time_t now;
   struct tm timeinfo;
   if (!getLocalTime(&timeinfo)) {
-    //Serial.println("Failed to obtain time");
     return(0);
   }
   time(&now);
@@ -27,18 +27,21 @@ unsigned long getTime() {
 }
 
 void api(){
-    // Your Domain name with URL path or IP address with path
+    //Lien de l'API
     http.begin("https://api.bagtower.bag-era.fr/prod/logs");
 
-    // Specify content-type header
+    //Configuration du Head
     http.addHeader("Content-Type", "application/json");
     http.addHeader("x-api-key", "7ce8BjFztCaFt5q9jdPYLaxtyagBh1xK8iaplrTF");
-    // Data to send with HTTP POST
+
+    //Création du JSON
+    //Création du dossier
     StaticJsonDocument<200> doc;
     doc["deviceId"] = "6f34c9b0-1791-1:3089fe40-3656-11ec-8";
     doc["unixTimestamp"] = getTime();
-
     JsonArray data = doc.createNestedArray("data");
+
+    //Création du sous-dossier
     StaticJsonDocument<200> sousDoc;
     sousDoc["id"] = "patate";
     sousDoc["type"] = "percent";
@@ -49,14 +52,17 @@ void api(){
     String requestBody;
     serializeJson(doc, requestBody);
 
-    // Send HTTP POST request
+    //Envoie d'un POST à requestBody
     int httpResponseCode = http.POST(requestBody);
+
+    //Affichage dans le monitor de la réponse de requestBody et httpResponseCode
     Serial.println(requestBody);
     Serial.println(httpResponseCode);
 
 }
 
 void setup(){
+    //Démarrage du M5
     Serial.begin(115200);
     M5.Lcd.setTextColor(WHITE);
     M5.Lcd.setTextSize(2);
@@ -67,6 +73,7 @@ void setup(){
 
     M5.Lcd.println("Connexion en cours, veuillez patienter.");
 
+    //Connexion au WiFi
     wifiConnect();
     while (WiFi.status() != 3) { 
         yield(); 
@@ -75,17 +82,21 @@ void setup(){
     M5.Lcd.setCursor(0, 0);
     M5.Lcd.println("Connexion réussi, vous avez accès à Internet.");
 
+    //Connexion au serveur NTP
     configTime(3600, 3600, "pool.ntp.org");
     struct tm timeinfo;
     getLocalTime(&timeinfo);
     M5.Lcd.println(&timeinfo,"%A, %B %d %Y %H:%M:%S");
 
-    
+    //Affichage de l'adresse IP 
     M5.Lcd.print("L'ip est: ");
     M5.Lcd.println(WiFi.localIP());
-    // delay(25000);
+    delay(10000);
 
     M5.Lcd.clear();
+    M5.Lcd.setCursor(0, 0);
+    M5.Lcd.setTextColor(GREEN);
+    M5.Lcd.println("Connexion API : OK");
     
     api();
 }
